f22ce227fcda91672bfc968494b0d7251f0b2e61
Angelica Mendez <angie.saudade@gmail.com>
2019-08-09 13:32:57 -0700

    added unity3 project

---
+++ Assets/ImpulseTester.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class ImpulseTester : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            GetComponent<CinemachineImpulseSource>().GenerateImpulse();
+        }
+    }
+
+}
---
+++ Assets/OnTriggerVolumeActions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class OnTriggerVolumeActions : MonoBehaviour
+{
+
+    [SerializeField]
+    private UnityEvent _OnTriggerEnter;
+
+    [SerializeField]
+    private UnityEvent _OnTriggerExit;
+
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _OnTriggerEnter.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _OnTriggerExit.Invoke();
+    }
+
+}
