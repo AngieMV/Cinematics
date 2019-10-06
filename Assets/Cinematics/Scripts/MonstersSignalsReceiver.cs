70ca499e4beaa0710d992df9be83b90b1128b426
Angelica Mendez <angie.saudade@gmail.com>
2019-10-06 09:38:27 -0700

    Added 30 seconds with Cyclopes animation

---
+++ Assets/Cinematics/Scripts/MonstersSignalsReceiver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MonstersSignalsReceiver : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void UpdateAnimation(string trigger)
+    {
+        animator.SetTrigger(trigger);
+    }
+
+   
+}
